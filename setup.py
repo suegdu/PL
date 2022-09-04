d1b136df1b1e440d75f04f663610f38e363a2696
human intellectual structure <91017265+suegdu@users.noreply.github.com>
2022-09-05 01:53:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@
 from setuptools import setup, find_packages
 
 
-VERSION = {}
-
-with open("./PL/__init__.py") as fp:
-    exec(fp.read(), VERSION)
 
 def read_requirements():
     with open('requirements.txt', 'r') as req:
@@ -19,7 +15,6 @@ setup(
     author_email="suebusiness@proton.me",
     url="https://github.com/suegdu/PL",
     description="Personal Python Package for Logging.",
-    version=VERSION.get("__version__", "0.0.0"),
     packages=find_packages(where=".", exclude=["tests"]),
     install_requires=read_requirements(),
     #install_requires=[
